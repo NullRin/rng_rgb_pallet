5227ad0927828755e2644c40659aa0d9e093d8b4
unknown <NullRin@users.noreply.github.com>
2024-05-28 18:18:28 -0700

    created rocket webapp impl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
+#[macro_use] extern crate rocket;
+
+use rocket::get;
+// use rocket::http::RawStr;
+// use rocket::request;
+use rocket::form::{self, FromForm, Error};
+// use std::default::Default;
 use rand::distributions::{Distribution, Uniform};
 use image::{RgbImage, Rgb};
 use std::path::Path;
+use rocket::fs::NamedFile;
+use rocket::response::status::NotFound;
 
 
 
+#[derive(Debug, FromForm)]
+struct Pallet {
+    #[field(default = 5)]
+    pallet_count: u32,
+    #[field(default = 128)]
+    pallet_size: u32,
+    #[field(default = 255)]
+    #[field(validate = validate_brightness(&self.min_brightness))]
+    max_brightness: u8,
+    #[field(default = 0)]
+    min_brightness: u8,
+}
 
-fn creat_pallet(pallet_count: &u32, pallet_size: &u32, max_brightness: &u8, min_brightness: &u8) {
+fn validate_brightness<'v>(max_brightness: &u8, min_brightness: &u8) -> form::Result<'v, ()> {
+    if max_brightness < min_brightness  {
+        Err(Error::validation("Invalid brightness levels. Max must be higher than min"))?;
+    }
+    Ok(())
+}
+
+
+fn create_pallet(pallet_count: &u32, pallet_size: &u32, max_brightness: &u8, min_brightness: &u8, path: &Path) {
     let height = pallet_size;
 
     let mut img = RgbImage::new(pallet_count * *height, *height);
@@ -23,18 +52,19 @@ fn creat_pallet(pallet_count: &u32, pallet_size: &u32, max_brightness: &u8, min_
             }   
         }        
     }
-    let path = Path::new("./test.png");
     img.save(path).unwrap();
+}
+
+#[get("/palette?<pallet..>")]
+async fn palette(pallet: Pallet) -> Result<NamedFile, NotFound<String>> {
+    let path = Path::new("./palette.png");
+    create_pallet(&pallet.pallet_count, &pallet.pallet_size, &pallet.max_brightness, &pallet.min_brightness, path);
+    NamedFile::open(&path).await.map_err(|e| NotFound(e.to_string()))
 }
 
 
 
-
-fn main() {
-    let pallet_count:u32 = 5;
-    let pallet_size:u32 = 128;
-    let max_brightness:u8 = 225;
-    let min_brightness:u8 = 0;
-    creat_pallet(&pallet_count, &pallet_size, &max_brightness, &min_brightness);
-
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![palette])
 }
