11ff786e6b484236f8fffdf93a28592f70eba43a
unknown <NullRin@users.noreply.github.com>
2024-05-30 16:39:00 -0700

    create ability to add height and width of pallet

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,10 @@ use rocket::response::status::NotFound;
 
 #[derive(Debug, FromForm)]
 struct Pallet {
-    #[field(default = 5)]
-    pallet_count: u32,
+    #[field(default = 1)]
+    pallet_height: u32,
+    #[field(default = 1)]
+    pallet_width: u32,
     #[field(default = 128)]
     pallet_size: u32,
     #[field(default = 255)]
@@ -34,23 +36,24 @@ fn validate_brightness<'v>(max_brightness: &u8, min_brightness: &u8) -> form::Re
 }
 
 
-fn create_pallet(pallet_count: &u32, pallet_size: &u32, max_brightness: &u8, min_brightness: &u8, path: &Path) {
-    let height = pallet_size;
-
-    let mut img = RgbImage::new(pallet_count * *height, *height);
+fn create_pallet(pallet_height: &u32, pallet_width: &u32, pallet_size: &u32, max_brightness: &u8, min_brightness: &u8, path: &Path) {
+    let mut img = RgbImage::new(pallet_width * pallet_size, pallet_height * pallet_size);
 
     let mut rng = rand::thread_rng();
     let die = Uniform::from(*min_brightness..=*max_brightness);
 
-    for pallet_number in 0..*pallet_count{
-        let red = die.sample(&mut rng);
-        let green = die.sample(&mut rng);
-        let blue = die.sample(&mut rng);
-        for x in 0..*pallet_size{
-            for y in 0..*pallet_size{
-                img.put_pixel(x+(pallet_number*pallet_size), y, Rgb([red, blue, green]));
-            }   
-        }        
+    
+    for vertical in 0..*pallet_height{
+        for horizontal in 0..*pallet_width{
+            let red = die.sample(&mut rng);
+            let green = die.sample(&mut rng);
+            let blue = die.sample(&mut rng);
+            for x in 0..*pallet_size{
+                for y in 0..*pallet_size{
+                    img.put_pixel(x+(horizontal*pallet_size), y+(vertical*pallet_size), Rgb([red, blue, green]));
+                }   
+            }        
+        }
     }
     img.save(path).unwrap();
 }
@@ -58,7 +61,7 @@ fn create_pallet(pallet_count: &u32, pallet_size: &u32, max_brightness: &u8, min
 #[get("/palette?<pallet..>")]
 async fn palette(pallet: Pallet) -> Result<NamedFile, NotFound<String>> {
     let path = Path::new("./palette.png");
-    create_pallet(&pallet.pallet_count, &pallet.pallet_size, &pallet.max_brightness, &pallet.min_brightness, path);
+    create_pallet(&pallet.pallet_height, &pallet.pallet_width, &pallet.pallet_size, &pallet.max_brightness, &pallet.min_brightness, path);
     NamedFile::open(&path).await.map_err(|e| NotFound(e.to_string()))
 }
 
